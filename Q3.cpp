8b5d150bd0b00ca9d487c199c8e5c706ef84626c
gArnavg <117974343+gArnavg@users.noreply.github.com>
2024-01-27 23:10:06 +0530

    Q3.cpp

---
+++ Q3.cpp
@@ -0,0 +1,26 @@
+//Q3 : Find the first non-repeating element in the array
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5]={1,2,2,4,7};
+    int n=5;
+    for (int i = 0; i < n; i++)
+    {
+         int j;
+        for (j = 0; j < n; j++)
+        {
+            if (i != j && arr[i] == arr[j])
+            {
+                break;
+            }
+            if (j == n)
+            {
+                cout<<arr[i];
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
